7494acca0fd8e1d2c2f6fd517856f037493a9e1a
sebastiangentini <102331445+sebastiangentini@users.noreply.github.com>
2022-10-25 14:11:46 -0300

    Initial commit

---
+++ Assets/Scripts/Hinoneko/ChangeSceneIntermedio.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeSceneIntermedio : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForChangeScene());
+    }
+
+    IEnumerator WaitForChangeScene()
+    {
+        yield return new WaitForSeconds(30);
+        ChangeScene();
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+}
